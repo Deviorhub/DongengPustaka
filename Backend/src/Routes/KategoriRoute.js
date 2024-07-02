36b21c559f19975bfa4b86a2be6b78586c8dfa8f
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-07-03 02:31:39 +0800

    Delete Backend

--- Backend/src/Routes/KategoriRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import {getAllKategorisController} from "../Controller/KategoriController.js"
-
-const router = express.Router()
-router.get("/kategoris", getAllKategorisController);
-router.get("/users/:id", getUsersById);
-
-export default router;
