34778211b9497a7cf0621190ffe8ffc9aa6822b5
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-07-02 16:59:49 +0800

    Changed BE

--- Backend/src/Controller/UserController.js
+++ Backend/src/Controller/UserController.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import {
   getAllUsers as modelGetAllUsers,
-  // getUsersById,
+  getUsersById as modelGetUsersId,
   // addUsers
 } from "../Models/ModelUsers.js";
 
@@ -18,41 +17,18 @@ export const getAllUsersController = async (req, res) => {
     });
   }
 };
-=======
 
-import { 
-    getAllUsers as modelGetAllUsers,
-    getUsersById as modelGetUsersId,
-    // addUsers
-} from "../Models/ModelUsers.js";
-
-export const getAllUsersController = async(req, res) => {
-    try {
-        const data = await modelGetAllUsers();
-        res.json({
-            message: 'GET semua users berhasil!',
-            data: data
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
-    }
-
-}
-
-export const getUsersById = async(req, res) => {
-    const {id} = req.params
-    try {
-        const data = await modelGetUsersId(id);
-        res.json({
-            message: 'GET users berhasil!',
-            data: data
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
-    }
-}
->>>>>>> a20f0aba758cbe18709c24dd50d05645f5809a0d
+export const getUsersById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await modelGetUsersId(id);
+    res.json({
+      message: "GET users berhasil!",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
