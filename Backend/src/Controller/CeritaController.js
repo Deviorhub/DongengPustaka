83892e5acda6153d429b7a866aef962fbb18b540
MrZwen <gedeagusadityadharma@gmail.com>
2024-07-03 01:30:52 +0800

    test

--- Backend/src/Controller/CeritaController.js
+++ Backend/src/Controller/CeritaController.js
@@ -25,8 +25,58 @@ export const getCeritaById = async (req, res) => {
   const { id } = req.params;
   try {
     const data = await modelGetCeritasId(id);
+    if (data.length > 0) {
+      res.json({
+        message: `GET cerita dengan ID:${id} berhasil!`,
+        data: data,
+      });
+    } else {
+      res.status(404).json({
+        message: `Cerita dengan ID:${id} tidak ditemukan.`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const createCeritaController = async (req, res) => {
+  try {
+    const data = await modelCreateCerita(req.body);
     res.json({
-      message: "GET kategoris berhasil!",
+      message: "POST cerita berhasil!",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const updateCeritaController = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await modelUpdateCerita(id, req.body);
+    res.json({
+      message: "PUT cerita berhasil!",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteCeritaController = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await modelDeleteCerita(id);
+    res.json({
+      message: "DELETE cerita berhasil!",
       data: data,
     });
   } catch (error) {
