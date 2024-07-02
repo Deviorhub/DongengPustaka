7f05a1dd662ef56109f7052f370fb65d62fc27d9
Ngurah Widi <widi050917@gmail.com>
2024-07-02 15:31:10 +0800

    User controller, user route

---
+++ Backend/src/Controller/UserController.js
@@ -0,0 +1,19 @@
+import {
+  getAllUsers as modelGetAllUsers,
+  // getUsersById,
+  // addUsers
+} from "../Models/ModelUsers.js";
+
+export const getAllUsersController = async (req, res) => {
+  try {
+    const data = await modelGetAllUsers();
+    res.json({
+      message: "GET semua users berhasil!",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
