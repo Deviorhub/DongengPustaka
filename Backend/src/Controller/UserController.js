7a8106f45d8eab681d84daf185a42e30bad3bdbc
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-07-02 16:55:56 +0800

    Changed BE

--- Backend/src/Controller/UserController.js
+++ Backend/src/Controller/UserController.js
@@ -1,20 +1,19 @@
-
-import { 
-    getAllUsers as modelGetAllUsers,
-    // getUsersById,
-    // addUsers
+import {
+  getAllUsers as modelGetAllUsers,
+  // getUsersById,
+  // addUsers
 } from "../Models/ModelUsers.js";
 
-export const getAllUsersController = async(req, res) => {
-    try {
-        const data = await modelGetAllUsers();
-        res.json({
-            message: 'GET semua users berhasil!',
-            data: data
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
-    }
-}
+export const getAllUsersController = async (req, res) => {
+  try {
+    const data = await modelGetAllUsers();
+    res.json({
+      message: "GET semua users berhasil!",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- Backend/src/Models/ModelUsers.js
+++ Backend/src/Models/ModelUsers.js
@@ -1,16 +1,15 @@
-
 import db from "../Config/Database.js";
 
-export const getAllUsers = async() => {
-    try {
-        const SQLQuery = 'SELECT * FROM users';
-        const [rows] = await db.execute(SQLQuery);
-        return rows;
-    } catch (error) {
-        console.error('Error pada proses getAllUsers:', error);
-        throw error;
-    }
-}
+export const getAllUsers = async () => {
+  try {
+    const SQLQuery = "SELECT * FROM users";
+    const [rows] = await db.execute(SQLQuery);
+    return rows;
+  } catch (error) {
+    console.error("Error pada proses getAllUsers:", error);
+    throw error;
+  }
+};
 
 // Komentar ini tetap dikomentari jika Anda tidak menggunakannya
 // export const getUsersById = async (id) => {
